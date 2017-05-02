8bc18df9ab2c9c33d9698a109bb4c94cb922219c
XierHacker <812760979@163.com>
2017-05-03 01:35:51 +0800

    initial commit

---
+++ TimeSeries/basic.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from datetime import timedelta
+
+#datetime
+now=datetime.now()
+print(now)
+print(type(now))
+print(now.year,now.month,now.day)
+
+
+#timedelta
---
+++ TimeSeries/basic2.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+dates=[datetime(2011,1,2),datetime(2011,1,3),datetime(2011,1,4),
+       datetime(2011, 1, 5),datetime(2011,1,6),datetime(2011,1,7)]
+
+data=range(6)
+
+
+
